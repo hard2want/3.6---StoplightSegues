f981151947473404404ab5e5ac6946b59bb003e5
hard2want <thedukeofchicago@gmail.com>
2018-06-14 18:56:18 -0500

    Completed the red / yellow / green view controllers and added the unwind segue from green back to red

--- 3.6 - StoplightSegues/ViewController.swift
+++ 3.6 - StoplightSegues/ViewController.swift
@@ -14,6 +14,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    
+    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue){
+        
+    } // end unwindToRed()
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
