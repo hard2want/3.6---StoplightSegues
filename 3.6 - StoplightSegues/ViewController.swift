f9de3c1e4ded3d65ac6da25ccb97bd4bab5e5936
hard2want <thedukeofchicago@gmail.com>
2018-06-14 19:20:44 -0500

    Completed passing information via text field on Red to modify the title of the yellow

--- 3.6 - StoplightSegues/ViewController.swift
+++ 3.6 - StoplightSegues/ViewController.swift
@@ -10,17 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    
-    
     @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue){
         
     } // end unwindToRed()
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.navigationItem.title = textField.text
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
