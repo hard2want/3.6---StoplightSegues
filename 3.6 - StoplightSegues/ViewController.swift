a08829ea5502622c42a1c617fce89ea87a3f30e4
hard2want <thedukeofchicago@gmail.com>
2018-06-14 19:27:39 -0500

    Removed the IBOutlet for the textField, removed the IBAction func unwindToRed(), removed override func prepare()

--- 3.6 - StoplightSegues/ViewController.swift
+++ 3.6 - StoplightSegues/ViewController.swift
@@ -10,21 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
-    @IBOutlet weak var textField: UITextField!
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue){
-        
-    } // end unwindToRed()
 
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        segue.destination.navigationItem.title = textField.text
-    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
